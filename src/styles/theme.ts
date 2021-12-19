a95dcf3baea302f243c8342c2f248c4b616d4cf7
Teilor Souza Barcelos <tsb-@live.com>
2021-12-19 14:27:50 -0300

    project config finished :tada:

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      '500': '#47585B',
+    },
+    yellow: {
+      '500': '#FFBA08'
+    },
+    white: {
+      '500': '#F5F8FA'
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white.500',
+        color: 'gray.500'
+      }
+    }
+  }
+})
