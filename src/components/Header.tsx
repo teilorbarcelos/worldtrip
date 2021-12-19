824f67559a00287162310a781ee9e4e7e823f4b0
Teilor Souza Barcelos <tsb-@live.com>
2021-12-19 16:10:13 -0300

    back button added to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,47 @@
-import { Flex, Image } from "@chakra-ui/react"
+import { Flex, Image, Link as ChakraLink } from "@chakra-ui/react"
+import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function Header() {
+  const { asPath } = useRouter()
 
   return (
     <Flex
       as="header"
       w='100%'
+      maxW={1160}
       h={[50, 100]}
-      justify="center"
+      mx="auto"
+      justify="space-between"
       align="center"
     >
+      {
+        asPath === '/' ?
+          <div /> :
+          <Link
+            href="/"
+            passHref
+          >
+            <ChakraLink
+              ml="16px"
+            >
+              <Image
+                src="/back.svg"
+                alt="Back button"
+                cursor="pointer"
+                w={["16px", "32px"]}
+              />
+            </ChakraLink>
+          </Link>
+      }
+
       <Image
         src="/logo.svg"
         alt="World Trip Logo"
         h={["20px", "45px"]}
       />
+
+      <div />
     </Flex>
   )
 }
