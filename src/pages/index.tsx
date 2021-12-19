dbacf9ac05d0897549a262098c660442500074fd
Teilor Souza Barcelos <tsb-@live.com>
2021-12-19 20:38:46 -0300

    travel types finished :tada:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import { Stack, Text } from '@chakra-ui/react'
+import { Flex, Stack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import TravelType from '../components/TravelType'
 
 const Home: NextPage = () => {
   return (
-    <Stack>
+    <Flex
+      direction="column"
+      justify="center"
+    >
       <Header />
       <Banner />
-    </Stack>
+      <TravelType />
+    </Flex>
   )
 }
 
